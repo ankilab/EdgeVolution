6c1f3214fd91ac4512e1505fc28eb8065ef24d0d
Rene Groh <iwb3022h@woody3.rrze.uni-erlangen.de>
2024-12-02 14:18:55 +0100

    Add script to determine baselines

---
+++ baselines/download_efficientnet.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+efficientnet = tf.keras.applications.EfficientNetB0(
+    include_top=False, 
+    weights='imagenet', 
+    input_shape=(224, 224, 3),
+    pooling='avg'
+)
+
+print(efficientnet.summary())
