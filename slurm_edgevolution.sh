a2db6b82f85d56e63b57c711e70c3f3621670750
rgroh1996 <rene.groh@fau.de>
2025-02-04 13:53:21 +0100

    Renamed some files

--- slurm_edgevolution.sh
+++ slurm_edgevolution.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=EvoNAS
+#SBATCH --job-name=EdgeVolution
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --output=R-%x.%j.out
@@ -18,12 +18,11 @@ module load python
 module load cuda
 module load cudnn
 
-conda activate evonas
+conda activate edgevolution
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORK/software/private/conda/envs/evonas/lib
 
-# srun python main.py +hyperparameters=emg_airob +search_space=emg_airob +boards=none
 srun python main.py +hyperparameters=speech_commands +search_space=speech_commands +boards=none
+# srun python main.py +hyperparameters=emg_airob +search_space=emg_airob +boards=none
 # srun python main.py +hyperparameters=cifar10 +search_space=cifar10 +boards=none
-
-# srun python continue_run.py --continue_path /home/woody/iwb3/iwb3022h/EvoNAS_Results/evonas_20241024-105453_cifar10 --continue_generation 13
+# srun python main.py +hyperparameters=daliac +search_space=daliac +boards=none
